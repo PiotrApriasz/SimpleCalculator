ebb32a6b91ad2e6a7fedac8b7ea412e8a67c7b3b
PiotrApriasz <piotrekapriasz@gmail.com>
2020-09-13 17:48:20 +0200

    First commit with code
    Add program name and possibility to enter numbers.
    Also implement function to check if user enters
    numbers. More in README.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            
+            int num1 = 0; int num2 = 0;
+
+            Console.WriteLine("Simple Console Calculator in C#\r");
+            Console.WriteLine("-------------------------------\n");
+
+            Console.Write("Enter first number: ");
+            num1 = NumbersCheck();
+
+            Console.Write("Enter second number: ");
+            num2 = NumbersCheck();
+        }
+
+        static int NumbersCheck()
+        {
+            Nullable<int> num = null;
+            while (true)
+            {
+                if (num != null)
+                {
+                    break;
+                }
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect numbers.");
+                    Console.Write("Try again: ");
+                    num = null;
+                }
+            }
+            return (int)num;
         }
     }
 }
